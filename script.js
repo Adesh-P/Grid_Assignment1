7a647d368e70f1e2c62a9c8e7a6314a49610fcd1
Adesh <pawase.adesh2016@gmail.com>
2023-02-03 18:01:20 +0530

    upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector('nav ul')
+let hamburger = document.querySelector('nav .hamburger')
+hamburger.addEventListener('click',function(){
+    menu.classList.toggle('show')
+})
